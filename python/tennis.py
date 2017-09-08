44b68c6cd3fc2347069dff9ed9bdfa6f7e57d99e
Seb Ashton <seb@sebashton.com>
2017-09-08 14:36:57 +0100

    Add points_to_score in MOAR places

--- python/tennis.py
+++ python/tennis.py
@@ -7,13 +7,13 @@ class TennisGame1:
         self.player2Name = player2Name
         self.p1points = 0
         self.p2points = 0
-        
+
     def won_point(self, playerName):
         if playerName == self.player1Name:
             self.p1points += 1
         else:
             self.p2points += 1
-    
+
     def score(self):
         result = ""
         tempScore=0
@@ -55,7 +55,7 @@ class TennisGame2:
         self.player2Name = player2Name
         self.p1points = 0
         self.p2points = 0
-        
+
     def won_point(self, playerName):
         if playerName == self.player1Name:
             self.P1Score()
@@ -71,7 +71,7 @@ class TennisGame2:
             return "Thirty"
         if (point==3):
             return "Forty"
-    
+
     def score(self):
         result = ""
         if (self.p1points == self.p2points and self.p1points < 3):
@@ -79,7 +79,7 @@ class TennisGame2:
             result += "-All"
         if (self.p1points==self.p2points and self.p1points>2):
             result = "Deuce"
-        
+
         P1res = ""
         P2res = ""
         if (self.p1points > 0 and self.p2points==0):
@@ -89,7 +89,7 @@ class TennisGame2:
                 P1res = "Thirty"
             if (self.p1points==3):
                 P1res = "Forty"
-            
+
             P2res = "Love"
             result = P1res + "-" + P2res
         if (self.p2points > 0 and self.p1points==0):
@@ -99,20 +99,22 @@ class TennisGame2:
                 P2res = "Thirty"
             if (self.p2points==3):
                 P2res = "Forty"
-            
+
             P1res = "Love"
             result = P1res + "-" + P2res
-        
-        
+
+
         if (self.p1points>self.p2points and self.p1points < 4):
-            if (self.p1points==2):
-                P1res="Thirty"
-            if (self.p1points==3):
-                P1res="Forty"
-            if (self.p2points==1):
-                P2res="Fifteen"
-            if (self.p2points==2):
-                P2res="Thirty"
+            P1res = self.points_to_score(self.p1points)
+            P2res = self.points_to_score(self.p2points)
+            # if (self.p1points==2):
+            #     P1res="Thirty"
+            # if (self.p1points==3):
+            #     P1res="Forty"
+            # if (self.p2points==1):
+            #     P2res="Fifteen"
+            # if (self.p2points==2):
+            #     P2res="Thirty"
             result = P1res + "-" + P2res
         if (self.p2points>self.p1points and self.p2points < 4):
             if (self.p2points==2):
@@ -124,47 +126,47 @@ class TennisGame2:
             if (self.p1points==2):
                 P1res="Thirty"
             result = P1res + "-" + P2res
-        
+
         if (self.p1points > self.p2points and self.p2points >= 3):
             result = "Advantage " + self.player1Name
-        
+
         if (self.p2points > self.p1points and self.p1points >= 3):
             result = "Advantage " + self.player2Name
-        
+
         if (self.p1points>=4 and self.p2points>=0 and (self.p1points-self.p2points)>=2):
             result = "Win for " + self.player1Name
         if (self.p2points>=4 and self.p1points>=0 and (self.p2points-self.p1points)>=2):
             result = "Win for " + self.player2Name
         return result
-    
+
     def SetP1Score(self, number):
         for i in range(number):
             self.P1Score()
-    
+
     def SetP2Score(self, number):
         for i in range(number):
             self.P2Score()
-    
+
     def P1Score(self):
         self.p1points +=1
-    
-    
+
+
     def P2Score(self):
         self.p2points +=1
-        
+
 class TennisGame3:
     def __init__(self, player1Name, player2Name):
         self.p1N = player1Name
         self.p2N = player2Name
         self.p1 = 0
         self.p2 = 0
-        
+
     def won_point(self, n):
         if n == self.p1N:
             self.p1 += 1
         else:
             self.p2 += 1
-    
+
     def score(self):
         if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
             p = ["Love", "Fifteen", "Thirty", "Forty"]
